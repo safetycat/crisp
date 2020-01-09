ccbaf26ba1132598c4b3e6758281b4470b93eb92
girl-elvis <theelvina@gmail.com>
2020-01-09 15:46:31 +0000

    move scss files to src, modify style.scss. Make changes to front page, header and footer

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import UIkit from 'uikit';
+import Icons from 'uikit/dist/js/uikit-icons';
+
+// loads the Icon plugin
+UIkit.use(Icons);
+require('./style.scss');
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'js/bundle.js'
+  },
+  module: {
+    rules: [
+      {
+      test: /\.scss$/,
+      use: ExtractTextPlugin.extract({
+        fallback: 'style-loader',
+        use: [
+          'css-loader',
+          'sass-loader'
+        ]
+        })
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {},
+          },
+        ],
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin('css/style.css'),
+  ]
+};
